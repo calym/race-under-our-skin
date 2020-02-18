3dab4a6926b03884eee7f1c4d552c9eef7034dce
unknown <cmcmorrow@smm.org>
2020-02-18 10:50:29 -0600

    text cleanup

--- src/component/About.js
+++ src/component/About.js
@@ -47,13 +47,13 @@ const About = (props) => {
       {langID === 'eng' && (
       <div>
         <div className="about-title" style={{ backgroundColor: theme.primary }}>
-        from
+        From
           <i>The Seattle Times</i>
         </div>
         <div className="about-content">
           <span className="big-quote-right" style={{ color: theme.primary }}>&ldquo;</span>
           <p className="quote-text">
-            Under Our Skin grew out of conversations about how we at The Seattle Times cover race at a time when national and local events — the furor over police shootings, the rise of the Black Lives Matter movement, protests on college campuses and charged campaign rhetoric — dominate headlines.
+            <i>Under Our Skin</i> grew out of conversations about how we at <i>The Seattle Times</i> cover race at a time when national and local events — the furor over police shootings, the rise of the Black Lives Matter movement, protests on college campuses and charged campaign rhetoric — dominate headlines.
           </p>
           <p className="quote-text">
             In our newsroom, we’ve found ourselves talking more candidly about race and racism, subjects that simmer beneath the surface even when they’re not on the front page.
@@ -64,12 +64,12 @@ const About = (props) => {
           <p className="quote-text ellipse">...</p>
           <p className="quote-text">
             Discussions about race, inclusiveness and sensitivity clearly aren’t new. They can leave us feeling depleted and wondering whether anything has really changed. But we believe the personal reflections and stories from the people who participated in this project will inspire all of us to think and talk about these issues in a deeper way. For those who freeze up at the prospect of talking about race, we hope this project will help break the ice. For
-            those who tend to take sides right away when the issue of race comes up, we hope Under Our Skin will challenge assumptions and build common ground.
+            those who tend to take sides right away when the issue of race comes up, we hope <i>Under Our Skin</i> will challenge assumptions and build common ground.
           </p>
           <p className="quote-text ellipse">...</p>
           <p className="quote-text">With many thanks to our 18 interviewees, and all of you,</p>
           <p className="quote-text">
-            The Under Our Skin team at
+            The <i>Under Our Skin</i> team at
             <i>The Seattle Times</i>
           </p>
           <p className="quote-text">Originally published June 20, 2016</p>
@@ -80,13 +80,13 @@ const About = (props) => {
       {langID === 'spa' && (
         <div>
           <div className="about-title" style={{ backgroundColor: theme.spaPrimary }}>
-            de
+            De
             <i>The Seattle Times</i>
           </div>
           <div className="about-content">
             <span className="big-quote-right" style={{ color: theme.spaPrimary }}>&ldquo;</span>
             <p className="quote-text">
-              Under Our Skin (Bajo nuestra piel) surgió de conversaciones acerca de cómo nosotros en The Seattle Times cubrimos temas sobre raza en un momento en que los sucesos nacionales y locales—el furor por los tiroteos policiales, el surgimiento del movimiento Black Lives Matter, las protestas en los campus universitarios y la retórica cargada de la campaña política—dominan los titulares.
+              <i>Under Our Skin (Bajo nuestra piel)</i> surgió de conversaciones acerca de cómo nosotros en <i>The Seattle Times</i> cubrimos temas sobre raza en un momento en que los sucesos nacionales y locales—el furor por los tiroteos policiales, el surgimiento del movimiento Black Lives Matter, las protestas en los campus universitarios y la retórica cargada de la campaña política—dominan los titulares.
             </p>
             <p className="quote-text">
               En nuestra sala de redacción, empezamos a hablar con más franqueza sobre la raza y el racismo, dos temas latentes incluso cuando no están en la primera plana.
@@ -100,11 +100,11 @@ const About = (props) => {
             </p>
             <p className="quote-text ellipse">. . .</p>
             <p className="quote-text">
-            Claramente, las discusiones sobre raza, inclusión y sensibilidad no son nuevas. Pueden hacer sentirnos agobiados y preguntándonos si algo realmente ha cambiado. Pero creemos que las reflexiones personales y las historias de las personas que participaron en este proyecto nos inspirarán a todos a pensar y hablar sobre estos temas de una manera más profunda. Para aquellos que se congelan ante la perspectiva de hablar sobre la raza, esperamos que este proyecto ayude a dar el primer paso. Para aquellos que tienden a tomar partido de inmediato cuando surge el tema de la raza, esperamos que Under Our Skin desafíe las suposiciones y se creen puntos en común.
+            Claramente, las discusiones sobre raza, inclusión y sensibilidad no son nuevas. Pueden hacer sentirnos agobiados y preguntándonos si algo realmente ha cambiado. Pero creemos que las reflexiones personales y las historias de las personas que participaron en este proyecto nos inspirarán a todos a pensar y hablar sobre estos temas de una manera más profunda. Para aquellos que se congelan ante la perspectiva de hablar sobre la raza, esperamos que este proyecto ayude a dar el primer paso. Para aquellos que tienden a tomar partido de inmediato cuando surge el tema de la raza, esperamos que <i>Under Our Skin</i> desafíe las suposiciones y se creen puntos en común.
             </p>
             <p className="quote-text ellipse">. . .</p>
             <p className="quote-text">Muchas gracias a nuestros 18 entrevistados, y a todos ustedes</p>
-            <p className="quote-text">El equipo Under Our Skin en The Seattle Times”</p>
+            <p className="quote-text">El equipo <i>Under Our Skin</i> en <i>The Seattle Times</i></p>
             <p className="quote-text">Publicado inicialmente el 20 de junio de 2016</p>
             <span className="big-quote-left" style={{ color: theme.spaPrimary }}>&rdquo;</span>
           </div>
